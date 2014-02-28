7ef8840aaf4373bba64ee7bc85d2d728875a5165
Marco Tedaldi <tedaldi@hifo.uzh.ch>
2014-02-28 14:32:37 +0100

    modified:   test_scripts/show_ip.py

--- test_scripts/show_ip.py
+++ test_scripts/show_ip.py
@@ -2,10 +2,18 @@
 
 import i2c_display
 import get_ip
+import smbus
 
+bus_nr = 1
+address = 0x3E
 
 def main():
-    ip = get_ip()
+    ip = get_ip.get_ip()
+    bus = smbus.SMBus(bus_nr)
+    i2c_display.init_display(bus, address)
+    i2c_display.display_write_string(bus, address, 0, ip)
+    return
+
 
 
 if __name__ == "__main__":
