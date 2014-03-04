b4216e253967fc38cc756d1e05361c41840236a8
Marco Tedaldi <tedaldi@hifo.uzh.ch>
2014-03-04 14:12:54 +0100

    created show_data.py as copy of show_ip.py

---
+++ test_scripts/show_data.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import i2c_display
+import get_ip
+import smbus
+
+bus_nr = 1
+address = 0x3E
+
+def main():
+    ip = get_ip.get_ip()
+    bus = smbus.SMBus(bus_nr)
+    i2c_display.init_display(bus, address)
+    i2c_display.display_write_string(bus, address, 0, ip)
+    content = "Hallo Martin"
+    i2c_display.display_write_string(bus, address, 1, content)
+    return
+
+
+
+if __name__ == "__main__":
+        main();
+
+
